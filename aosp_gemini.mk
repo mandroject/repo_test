2d4e40d5847bde80403982ddc67ef92ee9a724b7
Marulam Heryanto Samosir <marulam.samosir@gmail.com>
2017-09-19 20:39:12 +0700

    add vendor pure

--- aosp_gemini.mk
+++ aosp_gemini.mk
@@ -22,14 +22,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from gemini device
 $(call inherit-product, device/xiaomi/gemini/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Include pure telephony configuration
+include vendor/pure/configs/pure_phone.mk
 
 # Inherit arm64 phone gapps
 $(call inherit-product-if-exists, vendor/gapps/gapps.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_gemini
+PRODUCT_NAME := gemini
 PRODUCT_DEVICE := gemini
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI 5
