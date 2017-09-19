efe8d09de700ecda978cfe6c968e471f8e546662
Marulam Heryanto Samosir <marulam.samosir@gmail.com>
2017-09-19 20:51:24 +0700

    Init

--- device.mk
+++ device.mk
@@ -18,7 +18,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Get non-open-source specific aspects
-$(call inherit-product, vendor/xiaomi/msm8996-common/msm8996-common-vendor.mk)
+$(call inherit-product, vendor/xiaomi/gemini/gemini-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
